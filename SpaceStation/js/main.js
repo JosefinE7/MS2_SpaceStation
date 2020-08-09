1f3d9e678a6d1c6bf6301c74f670d2c8ebc1f130
JosefinE7 <fin.jos@hotmail.com>
2020-08-09 15:59:37 +0000

    Add tried to implement Magnific-popup to Gallery section fail

---
+++ SpaceStation/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  //code below copied from Magnific Popup
+
+    $(".gallery-section .grid .test-popup-link").magnificPopup({
+    type: "image",
+    gallery:{enabled:true}
+  });
+
+
+});
+
+
+//<link rel="stylesheet" href="SpaceStation/Magnific-popup/dist/magnific-popup.css">
+// <script src="SpaceStation/Magnific-popup/dist/magnific-popup.min.js"></script>
