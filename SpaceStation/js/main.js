5e3a5bfe9a2f1d7c977cec2105f6228a848f3bd8
JosefinE7 <fin.jos@hotmail.com>
2020-08-10 17:47:56 +0000

    Add ISS marker to Leaflet map

--- SpaceStation/js/main.js
+++ SpaceStation/js/main.js
@@ -1,19 +1,34 @@
 $(document).ready(function () {
-
+  
+  // making a map and tiles
   const mymap = L.map('iss-map').setView([0, 0], 1);
-
-  const attribution = 
+    const attribution = 
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
   const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';    
   const tiles = L.tileLayer(tileUrl,{ attribution });
   tiles.addTo(mymap);
 
+  // making a marker with a costume icon
+  var issIcon = L.icon({
+    iconUrl: 'SpaceStation/img/iss-map-icon.png',
+    iconSize: [50, 32],
+    iconAnchor: [25, 16],
+});
+
+  const marker = L.marker([0, 0], {icon: issIcon} ).addTo(mymap);
+
+
+  
+
   const iss_url = "https://api.wheretheiss.at/v1/satellites/25544"  
   async function getISS() {
       const response = await fetch(iss_url);
       const data = await response.json();
       const { latitude, longitude, altitude, velocity } = data;
+
+      //L.marker([latitude, longitude]).addTo(mymap);
+      marker.setLatLng([latitude, longitude]);
 
       document.getElementById("lat").textContent = latitude;
       document.getElementById("long").textContent = longitude;
