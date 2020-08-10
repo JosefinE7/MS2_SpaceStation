658391ed3403b1d3ddbdfed43a2762f8b19d75b9
JosefinE7 <fin.jos@hotmail.com>
2020-08-10 16:43:10 +0000

    Feat ISS Api connected

--- SpaceStation/js/main.js
+++ SpaceStation/js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function () {
-  //code below copied from Magnific Popup
+
+  const iss_url = "https://api.wheretheiss.at/v1/satellites/25544"  
+  async function getISS() {
+      const response = await fetch(iss_url);
+      const data = await response.json();
+      const { latitude, longitude, altitude, velocity } = data;
+
+      document.getElementById("lat").textContent = latitude;
+      document.getElementById("long").textContent = longitude;
+      document.getElementById("alt").textContent = altitude;
+      document.getElementById("vel").textContent = velocity;
+    }
+   
+  getISS();
+
+  
+    //code below copied from Magnific Popup
 
     $(".gallery-section .grid .test-popup-link").magnificPopup({
     type: "image",
