dbd3bb9166acf597f0b789bfe11aaf7bb1d5d2c5
JosefinE7 <fin.jos@hotmail.com>
2020-08-17 11:49:55 +0000

    Fix structure of functions in js file

--- SpaceStation/js/main.js
+++ SpaceStation/js/main.js
@@ -1,20 +1,24 @@
 // Start Location R-T section
 
 // making a map and tiles
-const MY_MAP = L.map("iss-map").setView([0, 0], 1);
-const ATTRIBUTION =
-	'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+function renderMap() {
+	const MY_MAP = L.map("iss-map").setView([0, 0], 1);
+	const ATTRIBUTION =
+		'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
-const TILE_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-const TILES = L.tileLayer(TILE_URL, { ATTRIBUTION });
-TILES.addTo(MY_MAP);
+	const TILE_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+	const TILES = L.tileLayer(TILE_URL, { ATTRIBUTION });
+	TILES.addTo(MY_MAP);
+}
 
 // making a marker with a custom icon
-var issIcon = L.icon({
-	iconUrl: "SpaceStation/img/iss-map-icon.png",
-	iconSize: [50, 32],
-	iconAnchor: [25, 16],
-});
+function addingCustomIcon() {
+	var issIcon = L.icon({
+		iconUrl: "SpaceStation/img/iss-map-icon.png",
+		iconSize: [50, 32],
+		iconAnchor: [25, 16],
+	});
+}
 
 const MARKER = L.marker([0, 0], { icon: issIcon }).addTo(MY_MAP);
 
@@ -73,5 +77,6 @@ $(".page-main .quotes-section .owl-carousel").owlCarousel({
 });
 
 $(document).ready(function () {});
+renderMap();
 //getISS();
 //setInterval(getISS, 1000);
