e92a7d7594dbc4cfcf5b3c9378ca56902343b1d8
JosefinE7 <fin.jos@hotmail.com>
2020-08-17 10:28:29 +0000

    Fix moved executing js code to the bottom of file

--- SpaceStation/js/main.js
+++ SpaceStation/js/main.js
@@ -1,81 +1,77 @@
-$(document).ready(function () {
-	// Start Location R-T section
+// Start Location R-T section
 
-	// making a map and tiles
-	const MY_MAP = L.map("iss-map").setView([0, 0], 1);
-	const ATTRIBUTION =
-		'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+// making a map and tiles
+const MY_MAP = L.map("iss-map").setView([0, 0], 1);
+const ATTRIBUTION =
+	'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
-	const TILE_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-	const TILES = L.tileLayer(TILE_URL, { ATTRIBUTION });
-	TILES.addTo(MY_MAP);
+const TILE_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const TILES = L.tileLayer(TILE_URL, { ATTRIBUTION });
+TILES.addTo(MY_MAP);
 
-	// making a marker with a custom icon
-	var issIcon = L.icon({
-		iconUrl: "SpaceStation/img/iss-map-icon.png",
-		iconSize: [50, 32],
-		iconAnchor: [25, 16],
-	});
+// making a marker with a custom icon
+var issIcon = L.icon({
+	iconUrl: "SpaceStation/img/iss-map-icon.png",
+	iconSize: [50, 32],
+	iconAnchor: [25, 16],
+});
 
-	const MARKER = L.marker([0, 0], { icon: issIcon }).addTo(MY_MAP);
+const MARKER = L.marker([0, 0], { icon: issIcon }).addTo(MY_MAP);
 
-	const ISS_URL = "https://api.wheretheiss.at/v1/satellites/25544";
+const ISS_URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
-	let firstTime = true;
-	async function getISS() {
-		const RESPONSE = await fetch(ISS_URL);
-		const DATA = await RESPONSE.json();
-		const { latitude, longitude, altitude, velocity } = DATA;
+let firstTime = true;
+async function getISS() {
+	const RESPONSE = await fetch(ISS_URL);
+	const DATA = await RESPONSE.json();
+	const { latitude, longitude, altitude, velocity } = DATA;
 
-		MARKER.setLatLng([latitude, longitude]);
-		if (firstTime) {
-			MY_MAP.setView([latitude, longitude], 2);
-			firstTime = false;
-		}
-
-		document.getElementById("lat").textContent = latitude.toFixed(3);
-		document.getElementById("long").textContent = longitude.toFixed(3);
-		document.getElementById("alt").textContent = altitude.toFixed(3);
-		document.getElementById("vel").textContent = velocity.toFixed(3);
+	MARKER.setLatLng([latitude, longitude]);
+	if (firstTime) {
+		MY_MAP.setView([latitude, longitude], 2);
+		firstTime = false;
 	}
 
-	//getISS(); <-- important
+	document.getElementById("lat").textContent = latitude.toFixed(3);
+	document.getElementById("long").textContent = longitude.toFixed(3);
+	document.getElementById("alt").textContent = altitude.toFixed(3);
+	document.getElementById("vel").textContent = velocity.toFixed(3);
+}
 
-	//setInterval(getISS, 1000);  <---important
+/**
+ * Code below is copied from Magnific Popup
+ * https://dimsemenov.com/plugins/magnific-popup/
+ * and it provides a "Lightbox Gallery" effect
+ * to gallery-section
+ *
+ */
 
-	// End Location R-T section
-
-	/**
-	 * Code below is copied from Magnific Popup
-	 * https://dimsemenov.com/plugins/magnific-popup/
-	 * and it provides a "Lightbox Gallery" effect
-	 * to gallery-section
-	 *
-	 */
-
-	$(".gallery-section .grid .test-popup-link").magnificPopup({
-		type: "image",
-		gallery: { enabled: true },
-	});
-
-	/**
-	 * Code below is copied from Owl Carousel
-	 * https://owlcarousel2.github.io/OwlCarousel2/demos/basic.html
-	 * and it provides a carousel effect to
-	 * quotes-section
-	 *
-	 */
-	$(".page-main .quotes-section .owl-carousel").owlCarousel({
-		loop: true,
-		autoplay: true,
-		dots: true,
-		responsive: {
-			0: {
-				items: 1,
-			},
-			544: {
-				items: 2,
-			},
-		},
-	});
+$(".gallery-section .grid .test-popup-link").magnificPopup({
+	type: "image",
+	gallery: { enabled: true },
 });
+
+/**
+ * Code below is copied from Owl Carousel
+ * https://owlcarousel2.github.io/OwlCarousel2/demos/basic.html
+ * and it provides a carousel effect to
+ * quotes-section
+ *
+ */
+$(".page-main .quotes-section .owl-carousel").owlCarousel({
+	loop: true,
+	autoplay: true,
+	dots: true,
+	responsive: {
+		0: {
+			items: 1,
+		},
+		544: {
+			items: 2,
+		},
+	},
+});
+
+$(document).ready(function () {});
+//getISS();
+//setInterval(getISS, 1000);
