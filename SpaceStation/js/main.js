3d6e1271d33b06af27fb47786bcd73501bf72d5a
JosefinE7 <fin.jos@hotmail.com>
2020-08-17 16:19:13 +0000

    Fixed decimal amount to const insted of numbers in code

--- SpaceStation/js/main.js
+++ SpaceStation/js/main.js
@@ -1,80 +1,95 @@
-$(document).ready(function () {
-	// Start Location R-T section
+/**
+ * Code below is copied from
+ * https://wheretheiss.at/
+ * in conjunction with a video from the
+ * channel The Coding Train
+ * https://www.youtube.com/watch?v=nZaZ2dB6pow&t=10s
+ * to provide a map and live updates provided by
+ * the iss api to the RT-section of page.
+ *
+ */
 
-	// making a map and tiles
-	const MY_MAP = L.map("iss-map").setView([0, 0], 1);
-	const ATTRIBUTION =
-		'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const MY_MAP = L.map("iss-map").setView([0, 0], 1); // When calling functions in Leaflet library you use L.
 
-	const TILE_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-	const TILES = L.tileLayer(TILE_URL, { ATTRIBUTION });
-	TILES.addTo(MY_MAP);
+/**  Whenever using anything based on OpenStreetMap, 
+an attribution is obligatory as per the copyright notice. */
+const ATTRIBUTION =
+	'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
-	// making a marker with a custom icon
-	var issIcon = L.icon({
-		iconUrl: "SpaceStation/img/iss-map-icon.png",
-		iconSize: [50, 32],
-		iconAnchor: [25, 16],
-	});
+const TILE_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const TILES = L.tileLayer(TILE_URL, { ATTRIBUTION });
+TILES.addTo(MY_MAP);
 
-	const MARKER = L.marker([0, 0], { icon: issIcon }).addTo(MY_MAP);
-
-	const ISS_URL = "https://api.wheretheiss.at/v1/satellites/25544";
-
-	let firstTime = true;
-	async function getISS() {
-		const RESPONSE = await fetch(ISS_URL);
-		const DATA = await RESPONSE.json();
-		const { latitude, longitude, altitude, velocity } = DATA;
-
-		MARKER.setLatLng([latitude, longitude]);
-		if (firstTime) {
-			MY_MAP.setView([latitude, longitude], 2);
-			firstTime = false;
-		}
-
-		document.getElementById("lat").textContent = latitude.toFixed(3);
-		document.getElementById("long").textContent = longitude.toFixed(3);
-		document.getElementById("alt").textContent = altitude.toFixed(3);
-		document.getElementById("vel").textContent = velocity.toFixed(3);
-	}
-	//getISS();
-	//setInterval(getISS, 1000);
-
-	/**
-	 * Code below is copied from Magnific Popup
-	 * https://dimsemenov.com/plugins/magnific-popup/
-	 * and it provides a "Lightbox Gallery" effect
-	 * to gallery-section
-	 *
-	 */
-
-	$(".gallery-section .grid .test-popup-link").magnificPopup({
-		type: "image",
-		gallery: { enabled: true },
-	});
-
-	/**
-	 * Code below is copied from Owl Carousel
-	 * https://owlcarousel2.github.io/OwlCarousel2/demos/basic.html
-	 * and it provides a carousel effect to
-	 * quotes-section
-	 *
-	 */
-	$(".page-main .quotes-section .owl-carousel").owlCarousel({
-		loop: true,
-		autoplay: true,
-		dots: true,
-		responsive: {
-			0: {
-				items: 1,
-			},
-			544: {
-				items: 2,
-			},
-		},
-	});
+/* Code below creates a marker with custom icon*/
+var issIcon = L.icon({
+	iconUrl: "SpaceStation/img/iss-map-icon.png",
+	iconSize: [50, 32],
+	iconAnchor: [25, 16],
 });
 
+const MARKER = L.marker([0, 0], { icon: issIcon }).addTo(MY_MAP);
+
+const ISS_URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+/** Code below fetches the information
+ * from the iss api and displays it
+ * on the page
+ */
+let firstTime = true;
+async function getISS() {
+	const RESPONSE = await fetch(ISS_URL);
+	const DATA = await RESPONSE.json();
+	const { latitude, longitude, altitude, velocity } = DATA;
+	const DECIMAL_AMOUNT = 3;
+
+	MARKER.setLatLng([latitude, longitude]);
+	if (firstTime) {
+		MY_MAP.setView([latitude, longitude], 2);
+		firstTime = false;
+	}
+
+	document.getElementById("lat").textContent = latitude.toFixed(DECIMAL_AMOUNT);
+	document.getElementById("long").textContent = longitude.toFixed(
+		DECIMAL_AMOUNT
+	);
+	document.getElementById("alt").textContent = altitude.toFixed(DECIMAL_AMOUNT);
+	document.getElementById("vel").textContent = velocity.toFixed(DECIMAL_AMOUNT);
+}
+
+/**
+ * Code below is copied from Magnific Popup
+ * https://dimsemenov.com/plugins/magnific-popup/
+ * and it provides a "Lightbox Gallery" effect
+ * to gallery-section
+ *
+ */
+
+$(".gallery-section .grid .test-popup-link").magnificPopup({
+	type: "image",
+	gallery: { enabled: true },
+});
+
+/**
+ * Code below is copied from Owl Carousel
+ * https://owlcarousel2.github.io/OwlCarousel2/demos/basic.html
+ * and it provides a carousel effect to
+ * quotes-section
+ *
+ */
+$(".page-main .quotes-section .owl-carousel").owlCarousel({
+	loop: true,
+	autoplay: true,
+	dots: true,
+	responsive: {
+		0: {
+			items: 1,
+		},
+		544: {
+			items: 2,
+		},
+	},
+});
+
+$(document).ready(function () {});
 //getISS();
 //setInterval(getISS, 1000);
